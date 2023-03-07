b90695e5058fc3eb1b2b5ceafbeca63c9e5a522b
diptichavan <diptiidforjob@gmail.com>
2023-03-07 08:11:46 +0000

    Terraform file changes

--- IaaS/main.tf
+++ IaaS/main.tf
@@ -1,128 +1,152 @@
 terraform {
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">3.0.0"
     }
+  }
 
-     docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.11.0"
-    }
+  backend "azurerm" {
+    storage_account_name = azurerm_storage_account.terraform_state_sa.name
+    container_name       = azurerm_storage_container.terraform_state_container.name
+    key                  = "terraform.tfstate"
+
+    access_key = data.azurerm_key_vault_secret.terraform_state_secret.value
   }
 }
-dhbfjd
-
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "d_rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-resource "azurerm_container_registry" "d_acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.d_rg.name
-  location            = azurerm_resource_group.d_rg.location
-  sku                 = "Basic"
-  admin_enabled       = true
-}
-resource "azurerm_container_group" "d_cont_grp" {
-  name                = "dipti-container-group"
-  location            = azurerm_resource_group.d_rg.location
-  resource_group_name = azurerm_resource_group.d_rg.name
-  ip_address_type     = "public"
-  dns_name_label      = "example-dns-name-label"
 
-  container {
-    name   = "example-container"
-    image  = "${azurerm_container_registry.d_acr.login_server}/example-image:latest"
-    cpu    = "1"
-    memory = "1.5"
-    ports {
-      port     = 80
-      protocol = "TCP"
-    }
-    environment_variables = {
-      EXAMPLE_ENV_VAR = "example-value"
-    }
+resource "azurerm_key_vault" "function_app_kv" {
+  name                = "state-keyvault"
+  location            = "west europe"
+  resource_group_name = azurerm_resource_group.example.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "get",
+      "list",
+    ]
+    secret_permissions = [
+      "get",
+      "list",
+    ]
   }
 }
 
+data "azurerm_key_vault_secret" "terraform_state_secret" {
+  name         = "state-secret"
+  key_vault_id = azurerm_key_vault.function_app_kv.id
+}
 
-resource "azurerm_storage_account" "d_storage" {
-  name                     = var.storage_name
-  resource_group_name      = azurerm_resource_group.d_rg.name
-  location                 = azurerm_resource_group.d_rg.location
+variable "password" {
+  default = data.azurerm_key_vault_secret.terraform_state_secret.value
+}
+
+
+resource "azurerm_storage_account" "terraform_state_sa" {
+  name                     = "terraformstatesa"
+  resource_group_name      = azurerm_resource_group.function_app_rg.name
+  location                 = "west europe"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+resource "azurerm_storage_container" "terraform_state_container" {
+  name                  = "terraformstatecontainer"
+  storage_account_name  = azurerm_storage_account.terraform_state_sa.name
+  container_access_type = "private"
+}
 
-resource "azurerm_app_service_plan" "d_app_plan" {
-  name                = var.app_plan_name
-  location            = azurerm_resource_group.d_rg.location
-  resource_group_name = azurerm_resource_group.d_rg.name
-  kind                = "FunctionApp"
-  reserved            = true
+
+
+
+
+
+resource "azurerm_app_service_plan" "function_app_plan" {
+  name                = "function-app-service-plan"
+  location            = azurerm_resource_group.function_app_rg.location
+  resource_group_name = azurerm_resource_group.function_app_rg.name
+
   sku {
-    tier = "Standard"
+    tier = "Standerd"
     size = "S1"
   }
 }
 
-resource "azurerm_function_app" "d_func_app" {
-  name                       = var.func_app_name
-  location                   = azurerm_resource_group.d_rg.location
-  resource_group_name        = azurerm_resource_group.d_rg.name
-  app_service_plan_id        = azurerm_app_service_plan.d_app_plan.id
-  storage_account_name       = azurerm_storage_account.d_storage.name
-  storage_account_access_key = azurerm_storage_account.d_storage.primary_access_key
-  version                    = "~2"
+resource "azurerm_container_registry" "function_registry" {
+  name                = "function-container-registry"
+  location            = azurerm_resource_group.function_app_rg.location
+  resource_group_name = azurerm_resource_group.function_app_rg.name
+  sku                 = "Basic"
+  admin_enabled       = true
+}
 
-  app_settings = {
-    FUNCTION_APP_EDIT_MODE                    = "readOnly"
-    https_only                                = true
-    FUNCTIONS_EXTENSION_VERSION              = "~2"
-    DOCKER_REGISTRY_SERVER_URL               = "${azurerm_container_registry.d_acr.login_server}"
-    DOCKER_REGISTRY_SERVER_USERNAME          = "${azurerm_container_registry.d_acr.admin_username}"
-    DOCKER_REGISTRY_SERVER_PASSWORD          = "${azurerm_container_registry.d_acr.admin_password}"
-    WEBSITE_CONTENTAZUREFILECONNECTIONSTRING = "${azurerm_storage_account.d_storage.primary_connection_string}"
-    WEBSITE_CONTENTSHARE                     = "${azurerm_storage_account.d_storage.name}"
-    FUNCTIONS_WORKER_RUNTIME                 = "custom"
-    WEBSITES_PORT              = "80"
-    WEBSITE_RUN_FROM_PACKAGE                 = "https://diptistorage.blob.core.windows.net/deployments/python-sample-vscode-flask-tutorial-master.zip"
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE       = false
-    FUNCTIONS_CUSTOM_HANDLER   = "DockerHubExampleApp:Run"
+resource "azurerm_storage_account" "function_storage_sa" {
+  name                     = "functionstoragesa"
+  resource_group_name      = azurerm_resource_group.function_app_rg.name
+  location                 = azurerm_resource_group.function_app_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  enable_https_traffic_only = true
+}
+
+
+
+resource "azurerm_function_app" "function_app" {
+  name                = "functionapp1"
+  location            = azurerm_resource_group.function_app_rg.location
+  resource_group_name = azurerm_resource_group.function_app_rg.name
+  app_service_plan_id = azurerm_app_service_plan.function_app_rg.id
+   storage_account_name       = azurerm_storage_account.function_storage_sa.name
+  storage_account_access_key = azurerm_storage_account.function_storage_sa.primary_access_key
+
+  site_config {
+     always_on        = true
+    linux_fx_version = "DOCKER|mcr.microsoft.com/azure-functions/python:3.0-python3.8-appservice"
   }
 
-  # site_config {
-  #   always_on        = true
-  #   linux_fx_version  = "DOCKER|${azurerm_container_registry.d_acr.login_server}/${var.image_name}:${var.image_tag}"
-    
-  # }
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://${azurerm_container_registry.function_registry.login_server}"
+    "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.function_registry.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.function_registry.admin_password
+  }
 
-   identity {
+  identity {
     type = "SystemAssigned"
   }
 
+  depends_on = [
+    azurerm_container_registry.function_registry,
+  ]
 }
 
-resource "azurerm_function_app_slot" "d_func_app_slot" {
-  name                = "d-function-app-slot"
-  location            = azurerm_resource_group.d_rg.location
-  resource_group_name = azurerm_resource_group.d_rg.name
-  app_service_plan_id = azurerm_app_service_plan.d_app_plan.id
-  function_app_id     = azurerm_function_app.d_func_app.id
+resource "null_resource" "example" {
+  triggers = {
+    dockerfile = filebase64("C:\Terraform On Azure\Devops-using-Terraform\python-sample-vscode-flask-tutorial-master\Dockerfile")
+  }
 
-  app_settings = {
-    DOCKER_REGISTRY_SERVER_URL = azurerm_container_registry.d_acr.login_server
-    DOCKER_REGISTRY_USERNAME   = azurerm_container_registry.d_acr.admin_username
-    DOCKER_REGISTRY_PASSWORD   = azurerm_container_registry.d_acr.admin_password
-    WEBSITES_PORT              = "80"
-    FUNCTIONS_WORKER_RUNTIME   = "custom"
-    FUNCTIONS_CUSTOM_HANDLER   = "DockerHubExampleApp:Run"
+  provisioner "local-exec" {
+    command = "echo '${base64decode(var.dockerfile)}' | docker build -t ${azurerm_container_registry.function_registry.login_server}/${azurerm_container_registry.function_registry.name}:${var.tag} -f - . && docker push ${azurerm_container_registry.function_registry.login_server}/${azurerm_container_registry.function_registry.name}:${var.tag}"
   }
 }
+
+variable "tag" {
+  default = "latest"
+}
+
+variable "dockerfile" {
+  default = "Dockerfile"
+}
+
+
+
 
